c4362af7d5a7503f2681683a54b7c34416740b3e
Ashot Khan-Aslanyan <ashotkhanaslanyan@gmail.com>
2024-09-07 15:35:11 +0400

    Assignment 0 update

--- Fork and Exec Programs/MultipleForkCalls.c
+++ Fork and Exec Programs/MultipleForkCalls.c
@@ -9,33 +9,33 @@ int main() {
 	printf("Im Initial Parent! PID:%d\n", getpid());
 
 	fork1 = fork();
-	if (fork1 > 0) {
-		printf("I'm the parent of fork1=%d, with PID:%d\n", fork1, getppid());
+	if (!fork1) {
+		printf("I am the fork1! PID:%d\n", getpid());
 	}
-	else if (!fork1) {
-		printf("I am the Child! PID:%d\n", fork1);
+	else if (fork1 > 0) {
+		printf("I'm the parent of fork1=%d, with PID:%d\n", fork1, getppid());
 	}
 	else if (fork1 < 0) {
 		perror("fork");
 	}
 
 	fork2 = fork();
-	if (fork2 > 0) {
-		printf("I'm the parent of fork2=%d, with PID:%d\n", fork2, getppid());
+	if (!fork2) {
+		printf("I am the fork2! PID:%d\n", getpid());
 	}
-	else if (!fork2) {
-		printf("I am the Child! PID:%d\n", fork2);
+	else if (fork2 > 0) {
+		printf("I'm the parent of fork2=%d, with PID:%d\n", fork2, getppid());
 	}
 	else if (fork2 < 0) {
 		perror("fork");
 	}
 
 	fork3 = fork();
-	if (fork3 > 0) {
-		printf("I'm the parent of fork3=%d, with PID:%d\n", fork3, getppid());
+	if (!fork3) {
+		printf("I am the fork3! PID:%d\n", getpid());
 	}
-	else if (!fork3) {
-		printf("I am the Child! PID:%d\n", fork3);
+	else if (fork3 > 0) {
+		printf("I'm the parent of fork3=%d, with PID:%d\n", fork3, getppid());
 	}
 	else if (fork3 < 0) {
 		perror("fork");
