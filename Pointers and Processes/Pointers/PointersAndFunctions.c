2f4f18b36a46dc4cd51f773f9b9cce318d5d4689
Ashot Khan-Aslanyan <ashotkhanaslanyan@gmail.com>
2024-09-15 13:12:29 +0400

    Assignment 3

---
+++ Pointers and Processes/Pointers/PointersAndFunctions.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    
+    int x = 10;
+    int y = 20;
+
+    printf("Values before swap, x: %d, y: %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("Values after swap, x: %d, y: %d\n", x, y);
+
+    return 0;
+}
